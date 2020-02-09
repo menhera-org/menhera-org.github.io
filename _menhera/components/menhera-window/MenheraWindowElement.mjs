44f771d5583d64d9f65387e5d11da163ac6d2b83
metastable-void <git@yuka.h09.eu>
2020-02-10 03:42:27 +0900

    menus

--- _menhera/components/menhera-window/MenheraWindowElement.mjs
+++ _menhera/components/menhera-window/MenheraWindowElement.mjs
@@ -36,74 +36,122 @@ export class MenheraWindowElement extends HTMLElement
 		const shadow = shadowObjects.get (this);
 		shadow.root = this.attachShadow ({mode: 'closed'});
 		
-		appendNewElement (shadow.root, 'link', {
-			rel: 'stylesheet',
-			href: '/_menhera/common/common.css',
-		});
+		appendNewElement (
+			shadow.root, 'link', {
+				rel: 'stylesheet',
+				href: '/_menhera/common/common.css',
+			}
+		);
 		
-		appendNewElement (shadow.root, 'link', {
-			rel: 'stylesheet',
-			href: '/_menhera/components/menhera-window/menhera-window.css',
-		});
+		appendNewElement (
+			shadow.root, 'link', {
+				rel: 'stylesheet',
+				href: '/_menhera/components/menhera-window/menhera-window.css',
+			}
+		);
 		
-		shadow.frame = appendNewElement (shadow.root, 'div', {
-			id: 'frame',
-		});
+		shadow.frame = appendNewElement (
+			shadow.root, 'div', {
+				id: 'frame',
+			}
+		);
 		
-		shadow.menubar = appendNewElement (shadow.root, 'div', {
-			id: 'menubar',
-			hidden: true,
-		});
+		shadow.menubar = appendNewElement (
+			shadow.root, 'div', {
+				id: 'menubar',
+				hidden: true,
+			}
+		);
 		
-		shadow.drawer = appendNewElement (shadow.root, 'div', {
-			id: 'drawer',
-			hidden: true,
-		});
+		shadow.drawer = appendNewElement (
+			shadow.root, 'div', {
+				id: 'drawer',
+				hidden: true,
+			}
+		);
 		
-		shadow.hiddenActivities = appendNewElement (shadow.root, 'div', {
-			id: 'hidden-activities',
-			hidden: true,
-		});
+		shadow.hiddenActivities = appendNewElement (
+			shadow.root, 'div', {
+				id: 'hidden-activities',
+				hidden: true,
+			}
+		);
 		
-		shadow.slotVisible = appendNewElement (shadow.frame, 'slot', {
-			name: 'visible-activity',
-		});
+		shadow.slotVisible = appendNewElement (
+			shadow.frame, 'slot', {
+				name: 'visible-activity',
+			}
+		);
 		
-		shadow.slotInvisible = appendNewElement (shadow.hiddenActivities, 'slot');
+		shadow.slotInvisible = appendNewElement (
+			shadow.hiddenActivities, 'slot'
+		);
 		
-		shadow.frameSplash = appendNewElement (shadow.slotVisible, 'div', {
-			id: 'frame-splash',
-			hidden: true,
-		});
+		shadow.frameSplash = appendNewElement (
+			shadow.slotVisible, 'div', {
+				id: 'frame-splash',
+				hidden: true,
+			}
+		);
 		
 		shadow.frameSplash.innerHTML = `<svg id='loading-logo' viewBox='0 0 256 256' xmlns='http://www.w3.org/2000/svg'><circle cx='128' cy='128' fill='none' r='96' stroke='#5eaf00' stroke-width='16'/><path d='M84,96 l8,8 l-16,64 m16,-64 h96 l-16,64 m-32,-64 l-16,64' fill='none' stroke='#5eaf00' stroke-linecap='round' stroke-linejoin='round' stroke-width='16'/></svg>`;
 		
-		shadow.toggleDrawer = appendNewElement (shadow.menubar, 'button', {
-			id: 'toggle-drawer',
-		});
+		shadow.toggleDrawer = appendNewElement (
+			shadow.menubar, 'button', {
+				id: 'toggle-drawer',
+			}
+		);
 		
-		shadow.menuHeading = appendNewElement (shadow.menubar, 'h1', {
-			id: 'menu-heading',
-		});
+		shadow.menuHeading = appendNewElement (
+			shadow.menubar, 'h1', {
+				id: 'menu-heading',
+			}
+		);
 		
 		shadow.menuHeading.textContent = 'Loading...';
 		
-		shadow.menuAccount = appendNewElement (shadow.menubar, 'a', {
-			id: 'menu-account',
-			href: '/login',
-		});
+		shadow.menuAccount = appendNewElement (
+			shadow.menubar, 'a', {
+				id: 'menu-account',
+				href: '/login',
+			}
+		);
 		
 		shadow.menuAccount.textContent = 'Account / Sign in';
 		
-		shadow.drawerHeading = appendNewElement (shadow.drawer, 'h2', {
-			id: 'drawer-heading',
-		});
+		shadow.drawerHeading = appendNewElement (
+			shadow.drawer, 'h2', {
+				id: 'drawer-heading',
+			}
+		);
 		
-		shadow.drawerSubheading = appendNewElement (shadow.drawer, 'h3', {
-			id: 'drawer-subheading',
-		});
+		shadow.drawerSubheading = appendNewElement (
+			shadow.drawer, 'h3', {
+				id: 'drawer-subheading',
+			}
+		);
+		
+		shadow.drawerMenu = appendNewElement (
+			shadow.drawer, 'nav', {
+				id: 'drawer-menu',
+			}
+		);
+		
+		shadow.drawerMenuGroups = appendNewElement (
+			shadow.drawerMenu, 'ul', {
+				id: 'drawer-menu-groups',
+			}
+		);
+		
+		shadow.drawerMenuGroupsMap = Object.create (null);
+		
+		shadow.toggleDrawer.addEventListener ('click'
+			, ev => this.toggleDrawer ());
 	}
 	
+	/**
+		Opens or closes the drawer.
+	*/
 	toggleDrawer ()
 	{
 		if (this.classList.contains ('drawer-toggled')) {
@@ -115,9 +163,11 @@ export class MenheraWindowElement extends HTMLElement
 	
 	connectedCallback ()
 	{
+		// Safeguard
+		if (!this.isConnected) return;
+		
 		const shadow = shadowObjects.get (this);
-		shadow.toggleDrawer.addEventListener ('click'
-			, ev => this.toggleDrawer ());
+		
 	}
 	
 	disconnectedCallback ()
@@ -130,29 +180,108 @@ export class MenheraWindowElement extends HTMLElement
 		
 	}
 	
-	attributeChangedCallback ()
+	attributeChangedCallback (aName, aOldValue, aNewValue)
 	{
 		
 	}
 	
-	addMenuGroup (aGroupName, aGroupLabel)
+	addMenuGroup (aGroupName)
 	{
+		const shadow = shadowObjects.get (this);
+		if (aGroupName in shadow.drawerMenuGroupsMap) {
+			return;
+		}
 		
+		const group = {};
+		
+		group.wrapperElement = appendNewElement (
+			shadow.drawerMenuGroups, 'li'
+		);
+		
+		group.labelElement = appendNewElement (
+			group.wrapperElement, 'div', {
+				className: 'label'
+			}
+		);
+		
+		group.listContainer = appendNewElement (
+			group.wrapperElement, 'ul'
+		);
+		
+		group.itemsMap = Object.create (null);
+		
+		shadow.drawerMenuGroupsMap[aGroupName] = group;
+	}
+	
+	setMenuGroupLabel (aGroupName, aGroupLabel)
+	{
+		const shadow = shadowObjects.get (this);
+		if (!(aGroupName in shadow.drawerMenuGroupsMap)) {
+			return false;
+		}
+		
+		const group = shadow.drawerMenuGroupsMap[aGroupName];
+		group.labelElement.textContent = aGroupLabel;
+		return true;
 	}
 	
 	removeMenuGroup (aGroupName)
 	{
+		const shadow = shadowObjects.get (this);
+		if (!(aGroupName in shadow.drawerMenuGroupsMap)) {
+			return;
+		}
 		
+		const group = shadow.drawerMenuGroupsMap[aGroupName];
+		group.wrapperElement.remove ();
+		delete shadow.drawerMenuGroupsMap[aGroupName];
 	}
 	
+	/**
+		Add or modify a menu item.
+	*/
 	addMenuItem (aGroupName, aItemName, aItemLabel, aTarget)
 	{
+		const shadow = shadowObjects.get (this);
+		if (!(aGroupName in shadow.drawerMenuGroupsMap)) {
+			return false;
+		}
 		
+		const group = shadow.drawerMenuGroupsMap[aGroupName];
+		const groupItem = {};
+		
+		groupItem.wrapperElement = appendNewElement (
+			group.listContainer, 'li'
+		);
+		
+		groupItem.anchorElement = appendNewElement (
+			groupItem.wrapperElement, 'a', {
+				href: aTarget
+			}
+		);
+		
+		groupItem.textContent = aItemLabel;
+		
+		group.itemsMap[aItemName] = groupItem;
+		return true;
 	}
 	
 	removeMenuItem (aGroupName, aItemName)
 	{
+		const shadow = shadowObjects.get (this);
+		if (!(aGroupName in shadow.drawerMenuGroupsMap)) {
+			return;
+		}
 		
+		const group = shadow.drawerMenuGroupsMap[aGroupName];
+		
+		if (!(aItemName in group.itemsMap)) {
+			return;
+		}
+		
+		const groupItem = group.itemsMap[aItemName];
+		groupItem.wrapperElement.remove ();
+		delete group.itemsMap[aItemName];
 	}
 }
 
